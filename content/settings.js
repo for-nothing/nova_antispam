3a46c3c7e7f379507de9ebda34792381b05a3dd7
for-nothing <8f7pfg@gmail.com>
2015-01-13 15:59:31 +0500

    Update settings.js

--- content/settings.js
+++ content/settings.js
@@ -1,6 +1,6 @@
-window.addEventListener("load",function(){ 
+window.addEventListener("load",function(){
 	var set_price = document.getElementById("setnvcPrice");
-	set_price.addEventListener("click",function(){
+	set_price.addEventListener("command",function(){
 		save_price();
 	},false);
 	var set__price = document.getElementById("set_nvcPrice");
@@ -8,7 +8,7 @@ window.addEventListener("load",function(){
 		save_price();
 	},false);
 	var set_addres = document.getElementById("setnvcAddres");
-	set_addres.addEventListener("click",function(){
+	set_addres.addEventListener("command",function(){
 		save_addres();
 	},false);
 	var set__addres = document.getElementById("set_nvcAddres");
@@ -16,7 +16,7 @@ window.addEventListener("load",function(){
 		save_addres();
 	},false);
 	var add_eml = document.getElementById("addToWhiteList");
-	add_eml.addEventListener("click",function(){
+	add_eml.addEventListener("command",function(){
 		save_eml();
 	},false);
 	var add__eml = document.getElementById("add_ToWhiteList");
@@ -24,7 +24,7 @@ window.addEventListener("load",function(){
 		save_eml();
 	},false);
 	var check_line = document.getElementById("removeUnpaidPosts");
-	check_line.addEventListener("click",function(){
+	check_line.addEventListener("command",function(){
 		save_check("remove_unpaidPosts");
 		check_mail(novaPanel);
 	},false);
@@ -34,7 +34,7 @@ window.addEventListener("load",function(){
 		check_mail(novaPanel);
 	},false);
 	var sync_book = document.getElementById("syncAddrBook");
-	sync_book.addEventListener("click",function(){
+	sync_book.addEventListener("command",function(){
 		save_sync("sync_AddrBook");
 	},false);
 	var sync__book = document.getElementById("sync_AddrBook");
@@ -42,31 +42,30 @@ window.addEventListener("load",function(){
 		save_sync("syncAddrBook");
 	},false);
 	var accManagement = document.getElementById("accManagement");
-	accManagement.addEventListener("click",function(){
+	accManagement.addEventListener("command",function(){
 		accIdNum = 0;
 		enbl_accnts = [];
-		acc_mngmnt();
+		acc_mngmnt(novaPanel);
 	}, false);
 	var acc_Management = document.getElementById("acc_Management");
-	accManagement.addEventListener("click",function(){
+	acc_Management.addEventListener("click",function(){
 		accIdNum = 0;
 		enbl_accnts = [];
-		acc_mngmnt();
+		acc_mngmnt(novaPanel);
 	}, false);
 }, false);
-
-window.addEventListener("unload",function(){ 
+window.addEventListener("unload",function(){
 	save_txid();
 	save_posts();
 }, false);
 
-function acc_mngmnt(){
+function acc_mngmnt(novaPanel){
 	var prompts = Components.classes["@mozilla.org/embedcomp/prompt-service;1"].getService(Components.interfaces.nsIPromptService);
 	var acctMgr = Components.classes["@mozilla.org/messenger/account-manager;1"].getService(Components.interfaces.nsIMsgAccountManager);
 	var identities = acctMgr.allIdentities;
 	if(accIdNum < identities.length){
 		var identity = identities.queryElementAt(accIdNum,Components.interfaces.nsIMsgIdentity);
-		var result = prompts.confirm(null, "управление аккаунтами", "На вашем клиенте thunderbird обнаружен аккаунт " + identity.email + ".Желаете использовать на нём nova_antispam?");
+		var result = prompts.confirm(null, account_management, detected_account + identity.email + like_to_use);
 		if(result){
 			enbl_accnts.push(identity.email);
 			accIdNum++;
@@ -85,12 +84,20 @@ function acc_mngmnt(){
 		var acc_write = enbl_accnts.join("\r\n");
 		var file = Components.classes["@mozilla.org/file/local;1"].createInstance(Components.interfaces.nsILocalFile);
 		var prof = Components.classes["@mozilla.org/file/directory_service;1"].getService(Components.interfaces.nsIProperties).get("ProfD", Components.interfaces.nsIFile);
-		file.initWithPath(prof.path + "\\accnts_save.txt");
+		if(prof.path.match("/")){
+			var slash = "/";
+		}
+		else{
+			var slash = "\\";
+		}
+		file.initWithPath(prof.path + slash + "accnts_save.txt");
 		var foStream = Components.classes["@mozilla.org/network/file-output-stream;1"].createInstance(Components.interfaces.nsIFileOutputStream);
 		foStream.init(file, 0x02 | 0x08 | 0x20, 0660, 0);
 		foStream.write(acc_write, acc_write.length);
 		foStream.close();
+		
 	}
+	check_mail(novaPanel);
 }
 
 function save_posts(){
@@ -98,14 +105,20 @@ function save_posts(){
 	var foStream = Components.classes["@mozilla.org/network/file-output-stream;1"].createInstance(Components.interfaces.nsIFileOutputStream);
 	var file = Components.classes["@mozilla.org/file/local;1"].createInstance(Components.interfaces.nsILocalFile);
 	var prof = Components.classes["@mozilla.org/file/directory_service;1"].getService(Components.interfaces.nsIProperties).get("ProfD", Components.interfaces.nsIFile);
-	file.initWithPath(prof.path + "\\posts_save.txt");
+		if(prof.path.match("/")){
+			var slash = "/";
+		}
+		else{
+			var slash = "\\";
+		}
+		file.initWithPath(prof.path + slash + "posts_save.txt");
 	if(file.exists()){
 		foStream.init(file, 0x02 | 0x20, 0660, null);
 		foStream.write(posts_write,posts_write.length);
 		foStream.close();
 	}
 	else{
-		foStream.init(file,  0x02 | 0x08 | 0x20, 0660, null);
+		foStream.init(file, 0x02 | 0x08 | 0x20, 0660, null);
 		foStream.write(posts_write,posts_write.length);
 		foStream.close();
 	}
@@ -116,31 +129,42 @@ function save_txid(){
 	var foStream = Components.classes["@mozilla.org/network/file-output-stream;1"].createInstance(Components.interfaces.nsIFileOutputStream);
 	var file = Components.classes["@mozilla.org/file/local;1"].createInstance(Components.interfaces.nsILocalFile);
 	var prof = Components.classes["@mozilla.org/file/directory_service;1"].getService(Components.interfaces.nsIProperties).get("ProfD", Components.interfaces.nsIFile);
-	file.initWithPath(prof.path + "\\txid_save.txt");
+		if(prof.path.match("/")){
+			var slash = "/";
+		}
+		else{
+			var slash = "\\";
+		}
+		file.initWithPath(prof.path + slash + "txid_save.txt");
 	if(file.exists()){
 		foStream.init(file, 0x02 | 0x20, 0660, null);
 		foStream.write(txid_write,txid_write.length);
 		foStream.close();
 	}
 	else{
-		foStream.init(file,  0x02 | 0x08 | 0x20, 0660, null);
+		foStream.init(file, 0x02 | 0x08 | 0x20, 0660, null);
 		foStream.write(txid_write,txid_write.length);
 		foStream.close();
 	}
-	
 }
 
 function save_price(){
 	var prompts = Components.classes["@mozilla.org/embedcomp/prompt-service;1"].getService(Components.interfaces.nsIPromptService);
 	var check = {value: false};
 	var input = {value: price};
-	var result = prompts.prompt(null, "количество nvc", "Введите требуемое количество nvc", input, null, check);
+	var result = prompts.prompt(null, amount_nvc, enter_amount, input, null, check);
 	if(result){
 		price = input.value.replace(",",".");
 		var price_write = "price=" + price ;
 		var file = Components.classes["@mozilla.org/file/local;1"].createInstance(Components.interfaces.nsILocalFile);
 		var prof = Components.classes["@mozilla.org/file/directory_service;1"].getService(Components.interfaces.nsIProperties).get("ProfD", Components.interfaces.nsIFile);
-		file.initWithPath(prof.path + "\\extensions\\user_save.txt");
+		if(prof.path.match("/")){
+			var slash = "/";
+		}
+		else{
+			var slash = "\\";
+		}
+		file.initWithPath(prof.path + slash + "user_save.txt");
 		var istream = Components.classes["@mozilla.org/network/file-input-stream;1"].createInstance(Components.interfaces.nsIFileInputStream);
 		istream.init(file, 0x01, 00660, null);
 		var mInputStream = Components.classes["@mozilla.org/scriptableinputstream;1"].createInstance(Components.interfaces.nsIScriptableInputStream);
@@ -159,14 +183,20 @@ function save_price(){
 function save_addres(){
 	var prompts = Components.classes["@mozilla.org/embedcomp/prompt-service;1"].getService(Components.interfaces.nsIPromptService);
 	var check = {value: false};
-	var input = {value: user_attr.addres};
-	var result = prompts.prompt(null, "адрес nvc", "Введите адрес nvc", input, null, check);
+	var input = {value: user_attr.address};
+	var result = prompts.prompt(null, address_nvc, enter_address_nvc, input, null, check);
 	if(result){
-		user_attr.addres = input.value;
-		var addres_write = "addres=" + user_attr.addres ;
+		user_attr.address = input.value;
+		var address_write = "address=" + user_attr.address ;
 		var file = Components.classes["@mozilla.org/file/local;1"].createInstance(Components.interfaces.nsILocalFile);
 		var prof = Components.classes["@mozilla.org/file/directory_service;1"].getService(Components.interfaces.nsIProperties).get("ProfD", Components.interfaces.nsIFile);
-		file.initWithPath(prof.path + "\\extensions\\user_save.txt");
+		if(prof.path.match("/")){
+			var slash = "/";
+		}
+		else{
+			var slash = "\\";
+		}
+		file.initWithPath(prof.path + slash + "user_save.txt");
 		var istream = Components.classes["@mozilla.org/network/file-input-stream;1"].createInstance(Components.interfaces.nsIFileInputStream);
 		istream.init(file, 0x01, 00660, null);
 		var mInputStream = Components.classes["@mozilla.org/scriptableinputstream;1"].createInstance(Components.interfaces.nsIScriptableInputStream);
@@ -174,7 +204,7 @@ function save_addres(){
 		var save_str = mInputStream.read(mInputStream.available());
 		istream.close();
 		mInputStream.close();
-		save_str = save_str.replace(/^addres=.*\w/gm,addres_write);
+		save_str = save_str.replace(/^addres+=.*\w/gm,address_write);
 		var foStream = Components.classes["@mozilla.org/network/file-output-stream;1"].createInstance(Components.interfaces.nsIFileOutputStream);
 		foStream.init(file, 0x02 | 0x20, 0660, null);
 		foStream.write(save_str,save_str.length);
@@ -186,14 +216,20 @@ function save_eml(){
 	var prompts = Components.classes["@mozilla.org/embedcomp/prompt-service;1"].getService(Components.interfaces.nsIPromptService);
 	var check = {value: false};
 	var input = {value: ""};
-	var result = prompts.prompt(null, "адрес email", "Введите адрес email(вы можете ввести несколько адресов,разделяя их пробелом", input, null, check);
+	var result = prompts.prompt(null, address_eml, enter_address_eml, input, null, check);
 	if(result){
 		var input_eml = input.value.match(/\w[а-яА-Яa-z0-9A-Z-_.]*@[а-яА-Яa-z0-9A-Z-_.]*\w/g);
 		white_list = white_list.concat(input_eml);
 		var eml_write = input_eml.join("\r\n");
 		var file = Components.classes["@mozilla.org/file/local;1"].createInstance(Components.interfaces.nsILocalFile);
 		var prof = Components.classes["@mozilla.org/file/directory_service;1"].getService(Components.interfaces.nsIProperties).get("ProfD", Components.interfaces.nsIFile);
-		file.initWithPath(prof.path + "\\extensions\\user_save.txt");
+		if(prof.path.match("/")){
+			var slash = "/";
+		}
+		else{
+			var slash = "\\";
+		}
+		file.initWithPath(prof.path + slash + "user_save.txt");
 		var foStream = Components.classes["@mozilla.org/network/file-output-stream;1"].createInstance(Components.interfaces.nsIFileOutputStream);
 		foStream.init(file, 0x02 | 0x10, 0660, null);
 		foStream.write(eml_write,eml_write.length);
@@ -203,7 +239,6 @@ function save_eml(){
 
 function save_check(id_name){
 	request_posts = [];
-//	paid_posts = [];
 	var check__line = document.getElementById(id_name);
 	if(save_ch == "false"){
 		save_ch = "true";
@@ -215,7 +250,13 @@ function save_check(id_name){
 	}
 	var file = Components.classes["@mozilla.org/file/local;1"].createInstance(Components.interfaces.nsILocalFile);
 	var prof = Components.classes["@mozilla.org/file/directory_service;1"].getService(Components.interfaces.nsIProperties).get("ProfD", Components.interfaces.nsIFile);
-	file.initWithPath(prof.path + "\\flag_save013.txt");
+		if(prof.path.match("/")){
+			var slash = "/";
+		}
+		else{
+			var slash = "\\";
+		}
+		file.initWithPath(prof.path + slash + "flag_save013.txt");
 	var istream = Components.classes["@mozilla.org/network/file-input-stream;1"].createInstance(Components.interfaces.nsIFileInputStream);
 	istream.init(file, 0x01, 00660, null);
 	var mInputStream = Components.classes["@mozilla.org/scriptableinputstream;1"].createInstance(Components.interfaces.nsIScriptableInputStream);
@@ -242,7 +283,13 @@ function save_sync(id_name){
 	}
 	var file = Components.classes["@mozilla.org/file/local;1"].createInstance(Components.interfaces.nsILocalFile);
 	var prof = Components.classes["@mozilla.org/file/directory_service;1"].getService(Components.interfaces.nsIProperties).get("ProfD", Components.interfaces.nsIFile);
-	file.initWithPath(prof.path + "\\flag_save013.txt");
+		if(prof.path.match("/")){
+			var slash = "/";
+		}
+		else{
+			var slash = "\\";
+		}
+		file.initWithPath(prof.path + slash + "flag_save013.txt");
 	var istream = Components.classes["@mozilla.org/network/file-input-stream;1"].createInstance(Components.interfaces.nsIFileInputStream);
 	istream.init(file, 0x01, 00660, null);
 	var mInputStream = Components.classes["@mozilla.org/scriptableinputstream;1"].createInstance(Components.interfaces.nsIScriptableInputStream);
